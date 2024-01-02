5d79d88f83c50849b780d8ff5eadb4ce0e4f2550
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2024-01-02 14:59:26 -0800

    different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for num in range(10):
     for next_num in range(num + 1, 10):
-        print("{:d}{:d}".format(num, next_num), end=(", " if num < 8 or next_num < 9 else "\n"))
+        print("{:d}{:d}".format(num, next_num),
+                end=(", " if num < 8 or next_num < 9 else "\n"))
