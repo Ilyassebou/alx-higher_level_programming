314e60aa3258308cedb3adfa5d7d01f4a1b4947f
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2024-01-04 08:08:58 -0800

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-         if name[0] != '_' and name[1] != '_':
+    for name in dir(hidden_4):
+        if not name.startswith("__"):
             print(name)
