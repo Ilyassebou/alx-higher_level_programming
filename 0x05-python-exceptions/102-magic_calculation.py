4af151dd18bdd0f05a54d66a3fa9535c70ce5eac
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2024-01-24 07:51:33 -0800

    CPython #2: PyFloatObject

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,12 +3,11 @@
 def magic_calculation(a, b):
     num = 0
     for i in range(1, 4):
-        if i > a:
-            try:
+        try:
+            if i > a:
                 raise Exception('Too far')
-            except Exception:
-                num += a + b
-                break
-        else:
             num += (a ** b) / i
+        except Exception:
+            num += a + b
+            break
     return num
