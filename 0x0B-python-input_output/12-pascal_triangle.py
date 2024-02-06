f7a322970e834c291e553024cdd47bdf3bce4d5a
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2024-02-06 12:01:32 -0800

    Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""pascal_triangle module"""
+
+
+def pascal_triangle(n):
+    """Represents Pascal's Triangle of size n"""
+    if n <= 0:
+        return []
+
+    result = [[1]]
+    while len(result) is not n:
+        tmp = [1]
+        for i in range(len(result[-1]) - 1):
+            tmp.append(result[-1][i] + result[-1][i + 1])
+        tmp.append(1)
+        result.append(tmp)
+    return result
