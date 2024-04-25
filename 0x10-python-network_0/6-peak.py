3f9e74d0130ef01ca1bff97eb4ecf01d98b8489a
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2024-04-25 12:38:31 -0700

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-""" Finds Peak values """
+"""Module that finds a peak in a list of integers"""
+
 
 def find_peak(list_of_integers):
-    """Find the peak"""
-    if not list_of_integers:
+    """function that finds a peak in a list of unsorted integers."""
+    list_ = list_of_integers
+    # if there is no list of integers return None
+    if list_ == []:
         return None
-    peak = binary_search(list_of_integers)
-    return list_of_integers[peak]
+    length = len(list_)
 
-""" binary search algorithm """
-
-def binary_search(a):
-    """Iterative binary search of the peak"""
-    lo, hi = 0, len(a) - 1
-    while lo < hi:
-        mid = (hi - lo) // 2 + lo
-        if a[mid] > a[mid + 1]:
-            hi = mid
+    start, end = 0, length - 1
+    while start < end:
+        mid = start + (end - start) // 2
+        if list_[mid] > list_[mid - 1] and list_[mid] > list_[mid + 1]:
+            return list_[mid]
+        if list_[mid - 1] > list_[mid + 1]:
+            end = mid
         else:
-            lo = mid + 1
-    return lo
+            start = mid + 1
+    return list_[start]
