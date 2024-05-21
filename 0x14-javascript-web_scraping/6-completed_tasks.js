417aa531444a98105f500b577fe44759e8c1fac3
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2024-05-21 12:33:55 -0700

    How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  let tasks = JSON.parse(body);
+  let obj = {};
+  for (let task of tasks) {
+    if (task.completed === true) {
+      if (obj[task.userId] === undefined) {
+        obj[task.userId] = 1;
+      } else {
+        obj[task.userId]++;
+      }
+    }
+  }
+  console.log(obj);
+});
